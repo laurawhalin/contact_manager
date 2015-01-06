c3edf599f7a8dce0c8309f6fd7c64b727354da28
Laura Whalin <laurawhalin@gmail.com>
2015-01-06 13:53:24 -0700

    added Person model with scaffolding

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # Without the following line, you may have diddiculty deleting once deployed to Heroku.
+  config.serve_static_assets = true
 end
