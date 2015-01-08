24035e402dca28ec9cc858bd5439efdf61889b65
Laura Whalin <laurawhalin@gmail.com>
2015-01-07 17:27:02 -0700

    made sure a company is valid with name

--- spec/models/company_spec.rb
+++ spec/models/company_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Company, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:company) { Company.create(name: 'Rainbow Socks Co.')}
+
+  it 'is valid' do
+    expect(company).to be_valid
+  end
+
+  it 'requires a name to be valid' do
+    company = Company.new
+    expect(company).to_not be_valid
+  end
+
 end
