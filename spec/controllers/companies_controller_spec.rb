732f935264b3c3dffb2251e42760d8d72cb8e4ac
Laura Whalin <laurawhalin@gmail.com>
2015-01-08 07:09:24 -0700

    updated companies controller spec to not have failing tests

--- spec/controllers/companies_controller_spec.rb
+++ spec/controllers/companies_controller_spec.rb
@@ -24,11 +24,11 @@ RSpec.describe CompaniesController, :type => :controller do
   # Company. As you add validations to Company, be sure to
   # adjust the attributes here as well.
   let(:valid_attributes) {
-    { name: "Pickle Company", contact_id: 1, contact_type: 'Business'}
+    { name: "Pickle Company"}
   }
 
   let(:invalid_attributes) {
-    { name: nil, contact_id: nil, contact_type: nil }
+    { name: nil }
   }
 
   # This should return the minimal set of values that should be in the session
